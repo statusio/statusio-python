c8164ecc322402023e36143ed9bd2f15c3233835
Joe Nicosia <joe@t3ch.com>
2017-12-14 17:52:25 -0500

    Update Python package to version 0.0.5

--- statusio/__init__.py
+++ statusio/__init__.py
@@ -3,8 +3,8 @@
 """A library that provides a Python interface to the Status.io API"""
 from __future__ import absolute_import
 
-__author__ = 'support@status.io'
-__version__ = '0.4'
+__author__ = 'hello@status.io'
+__version__ = '0.5'
 
 import json                                 # noqa
 
